fa45b03ab558cf21f9e9967e53710f2dec52a795
Алексей <itachi13aaaa@gmail.com>
2026-04-28 21:20:50 +0300

    first commit

---
+++ src/components/Chat/ChatInput/ChatInput.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { ref } from 'vue';
+    const message = ref('');
+
+</script>
+<template>
+    <div class="chat-input">
+        <textarea class="chat-input__textarea" id="new-message" v-model="message" placeholder="Напишите сообщение...">
+
+        </textarea>
+        <button class="chat-input__button">
+            <svg width="42" height="34" viewBox="0 0 42 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M42 0L33.6507 33.3989L19.3598 22.0709L34.1741 7.23366L11.6282 18.5066L0 13.9554L42 0Z" fill="#168DD9"/>
+            <path d="M12.835 20.6551L16.8989 32.2743V21.053C22.5557 15.3876 21.6231 16.3215 21.7437 16.2007C19.088 17.5286 15.779 19.1831 12.835 20.6551Z" fill="#168DD9"/>
+            <path d="M23.1747 28.2355L19.3594 32.274V25.2109L23.1747 28.2355Z" fill="#168DD9"/>
+            </svg>
+        </button>
+    </div>
+</template>
+<style src="./__chat-input.css" lang="css" />
---
+++ src/components/Chat/ChatPopup/ChatPopup.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ChatInput from '../ChatInput/ChatInput.vue';
+</script>
+<template>
+    <div class="chat-popup">
+        <div class="chat-popup__inner">
+            <div class="chat-popup__header">
+                <div class="chat-popup__title"><span>Ч</span>ат бот</div>
+            </div>
+            <div class="chat-popup__body">
+                <div class="chat-popup__messeges">
+                    <div class="chat-popup__messege --bot">hello world</div>
+                    <div class="chat-popup__messege">hello world</div>
+                </div>
+                <ChatInput />
+            </div>
+        </div>
+        <div class="chat-popup__close">
+            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M26 2L14 14M2 26L14 14M14 14L2 2L26 26" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+    </div>
+</template>
+<style src="./__chat-popup.css" lang="css" />
