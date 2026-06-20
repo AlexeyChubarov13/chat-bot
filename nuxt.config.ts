b2500e39cfdbf21ba4795e62ace63e2e0931c1e1
ertanfird <ertanfird@gmail.com>
2026-06-20 19:42:32 +0300

    delete garbage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2026-06-16',
-  modules: ['@pinia/nuxt'],
-  css: ['~/assets/style.css'],
-  runtimeConfig: {
-    dadataApiKey: process.env.DADATA_API_KEY || ''
-  }
+    compatibilityDate: '2026-06-16',
+    modules: ['@pinia/nuxt'],
+    css: ['~/assets/style.css'],
+    devServer: {
+        host: '0.0.0.0'
+    },
+    runtimeConfig: {
+        dadataApiKey: process.env.DADATA_API_KEY || ''
+    }
 })
