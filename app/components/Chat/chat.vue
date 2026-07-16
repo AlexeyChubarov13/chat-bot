1c5d36bf71cd671ad48f995ac05dfaa599e2189e
Алексей <itachi13aaaa@gmail.com>
2026-07-16 14:32:19 +0300

    scss

---
+++ app/components/Chat/chat.vue
@@ -0,0 +1,15 @@
+<script setup>
+const isOpen = ref(false)
+</script>
+
+<template>
+  <div class="chat">
+    <button class="chat__button" type="button" @click="isOpen = !isOpen">
+      <Icon name="ui:logo" size="22" />
+    </button>
+
+    <ChatPopup v-if="isOpen" @close="isOpen = false" />
+  </div>
+</template>
+
+<style src="./__chat.css"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 export default defineNuxtConfig({
     compatibilityDate: '2026-06-16',
-    modules: ['@pinia/nuxt'],
+    modules: ['@pinia/nuxt', '@nuxt/icon' ],
     css: ['~/assets/style.css'],
     devServer: {
         host: '0.0.0.0'
+    },
+    icon: {
+        mode: 'svg',
+        customCollections: [
+          {
+            prefix: 'ui',
+            dir: './app/assets/icons/ui'
+          }
+        ]
     },
     runtimeConfig: {
         dadataApiKey: process.env.DADATA_API_KEY || ''
